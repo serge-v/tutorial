3160a2d448ca1dc7bbeaaab15c81760d67fc8f3b
Serge Voilokov <serge0x76@gmail.com>
2016-10-18 18:06:55 -0400

    Working on x2

--- content/x2/helloworld.go
+++ content/x2/helloworld.go
@@ -5,21 +5,27 @@ import (
 	"net/http"
 	"net/http/cgi"
 	"log"
+	"flag"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello world\n")
+	fmt.Fprintf(w, "hello world. local = %v\n", *local)
 }
 
+var local = flag.Bool("local", false, "start local server for debugging")
+
 func main() {
-	if false {
+	flag.Parse()
+
+	if *local {
 		fmt.Println("=== starting server on http://localhost:8085/ ===")
 		http.HandleFunc("/", hello)
 		log.Fatal(http.ListenAndServe(":8085", nil))
-	else {
-		err := cgi.Serve(http.HandlerFunc(hello))
-		if err != nil {
-			panic(err)
-		}
+		return
+	}
+	
+	err := cgi.Serve(http.HandlerFunc(hello))
+	if err != nil {
+		panic(err)
 	}
 }
